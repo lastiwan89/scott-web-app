2f6068f7097c5ee135a96bc7095ffcfd43b9a304
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-03-08 11:52:34 +0900

    navbar update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,42 +1,104 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import Logo from "./logo";
-import Hamburger from "./hamburger";
 
 export default function Navbar() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  function handleOpen() {
+    setOpen((prev) => !prev);
+  }
+
   return (
-    <nav className="flex items-center justify-between gap-x-16 px-8 py-6 lg:px-40">
-      <Hamburger />
-      <div className="flex items-center gap-x-14">
+    // logo position need to fix
+    <nav className="relative flex items-center justify-center gap-x-16 px-8 py-6 md:justify-between lg:px-40">
+      <div className="absolute left-8 top-8 md:hidden" onClick={handleOpen}>
+        {!open ? (
+          <Image
+            src="/icons/hamburger.svg"
+            width={20}
+            height={16}
+            alt="Hamburger"
+          />
+        ) : (
+          <Image
+            src="/icons/close.svg"
+            width={19.69}
+            height={16}
+            alt="close icon"
+          />
+        )}
+      </div>
+
+      <div className="flex items-center md:justify-start md:gap-14">
         <Link href="/">
           <Logo />
         </Link>
-        <div className="hidden gap-8 md:flex">
-          <Link
-            className="font-mono text-15 font-bold text-grey-dimmed"
-            href="/about"
-          >
-            About
-          </Link>
-          <Link
-            className="font-mono text-15 font-bold text-grey-dimmed"
-            href="/location"
-          >
-            Location
-          </Link>
-          <Link
-            className="font-mono text-15 font-bold text-grey-dimmed"
-            href="/career"
-          >
-            Careers
+        <div className="hidden flex-col justify-start md:flex md:flex-row md:justify-start">
+          <div className="flex flex-col items-center gap-8 md:flex-row">
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/location"
+            >
+              Location
+            </Link>
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/career"
+            >
+              Careers
+            </Link>
+          </div>
+
+          <Link className="ml-auto" href="/career">
+            <button className="bg-yellow-dark px-9 py-3 font-mono text-15 font-bold text-grey-snow">
+              Get Scootin
+            </button>
           </Link>
         </div>
       </div>
-      <Link className="hidden md:block" href="/career">
-        <button className="bg-yellow-dark px-9 py-3 font-mono text-15 font-bold text-grey-snow">
-          Get Scootin
-        </button>
-      </Link>
+
+      {/* overlay nav */}
+      <div
+        className={`${open ? "fixed" : "hidden"} top-[77px] h-full w-full bg-grey-darker/40`}
+      >
+        <div className="flex h-full w-2/3 flex-col gap-80 bg-grey-darker pb-6 pl-8 pt-16">
+          <div className="flex flex-col gap-6">
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/location"
+            >
+              Location
+            </Link>
+            <Link
+              className="font-mono text-15 font-bold text-grey-dimmed"
+              href="/career"
+            >
+              Careers
+            </Link>
+          </div>
+
+          <Link className="" href="/career">
+            <button className="bg-yellow-dark px-9 py-3 font-mono text-15 font-bold text-grey-snow">
+              Get Scootin
+            </button>
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
